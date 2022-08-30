4ea9d31051479172ff689adb9fc0fc417e280f81
enuar rosales <enuaremiliorosales@gmail.com>
2022-08-30 09:23:34 -0500

    se agrega carpetas controladroes y servicios

---
+++ src/main/java/com/CGDJ6/transportes/controllers/ControlKilometrajeController.java
@@ -0,0 +1,4 @@
+package com.CGDJ6.transportes.controllers;
+
+public class ControlKilometrajeController {
+}
---
+++ src/main/java/com/CGDJ6/transportes/services/ControlKilometrajeService.java
@@ -0,0 +1,4 @@
+package com.CGDJ6.transportes.services;
+
+public class ControlKilometrajeService {
+}
